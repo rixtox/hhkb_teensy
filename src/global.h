034b5d7d5059b9c353a2f36f0460f2f051070af3
Rix <i@rix.li>
2015-10-04 00:20:35 -0400

    mv macros

--- src/global.h
+++ src/global.h
@@ -19,20 +19,6 @@
 #define SET_BIT(var,pos) ((var) |= (1<<(pos)))
 #define CLR_BIT(var,pos) ((var) &= ~(1<<(pos)))
 
-#define KEY_SELECT(row, col) \
-  digitalWrite(ROWA, CHECK_BIT(row, 0));\
-  digitalWrite(ROWB, CHECK_BIT(row, 1));\
-  digitalWrite(ROWC, CHECK_BIT(row, 2));\
-  digitalWrite(COLA, CHECK_BIT(col, 0));\
-  digitalWrite(COLB, CHECK_BIT(col, 1));\
-  digitalWrite(COLC, CHECK_BIT(col, 2))\
-
-#define KEY_PREV_ON() digitalWrite(HYS, HIGH)
-#define KEY_PREV_OFF() digitalWrite(HYS, LOW)
-#define KEY_ENABLE() digitalWrite(ENAB, LOW)
-#define KEY_UNABLE() digitalWrite(ENAB, HIGH)
-#define KEY_STATE() digitalRead(KEY)
-
 typedef struct keypos_t {
   int row;
   int col;
--- src/matrix.h
+++ src/matrix.h
@@ -3,5 +3,19 @@
 #endif
 #include "action.h"
 
+#define KEY_SELECT(row, col) \
+  digitalWrite(ROWA, CHECK_BIT(row, 0));\
+  digitalWrite(ROWB, CHECK_BIT(row, 1));\
+  digitalWrite(ROWC, CHECK_BIT(row, 2));\
+  digitalWrite(COLA, CHECK_BIT(col, 0));\
+  digitalWrite(COLB, CHECK_BIT(col, 1));\
+  digitalWrite(COLC, CHECK_BIT(col, 2))\
+
+#define KEY_PREV_ON() digitalWrite(HYS, HIGH)
+#define KEY_PREV_OFF() digitalWrite(HYS, LOW)
+#define KEY_ENABLE() digitalWrite(ENAB, LOW)
+#define KEY_UNABLE() digitalWrite(ENAB, HIGH)
+#define KEY_STATE() digitalRead(KEY)
+
 void matrix_scan();
 void matrix_exec();
